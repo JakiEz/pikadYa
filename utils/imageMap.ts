2513254a8ef4a5321039c6a6cae240e372f4bb60
Kittithat17 <hdhsdhdhdd1@gmail.com>
2026-04-05 19:12:18 +0700

    ss

--- utils/imageMap.ts
+++ utils/imageMap.ts
@@ -1,10 +1,12 @@
 //app/utils/imageMap.ts
 import { imageMap10 } from "./imageMap10";
+import { imageMap7 } from "./imageMap7";
 import { imageMap9 } from "./imageMap9";
 import { imageMaps } from "./imageMaps";
 
 export const imageMap = {
   ...imageMap10,
   ...imageMaps,
-  ...imageMap9
+  ...imageMap9,
+  ...imageMap7,
 };
---
+++ utils/imageMap7.ts
@@ -0,0 +1,20 @@
+export const imageMap7: Record<string, any> = {
+
+//พิกัดสัตตะโลหะ
+    "รากทองกวาว.jpg": require("@/assets/images/7/พิกัดสัตตะโลหะ/รากทองกวาว.jpg"),
+  "รากทองหลางหนาม.jpg": require("@/assets/images/7/พิกัดสัตตะโลหะ/รากทองหลางหนาม.jpg"),
+  "รากทองหลางใบมน.jpg": require("@/assets/images/7/พิกัดสัตตะโลหะ/รากทองหลางใบมน.jpg"),
+  "รากทองโหลง.jpg": require("@/assets/images/7/พิกัดสัตตะโลหะ/รากทองโหลง.jpg"),
+  "รากทองพันชั่ง.jpg": require("@/assets/images/7/พิกัดสัตตะโลหะ/รากทองพันชั่ง.jpg"),
+  "รากฟักทอง.jpg": require("@/assets/images/7/พิกัดสัตตะโลหะ/รากฟักทอง.jpg"),
+  "รากต้นใบทอง.jpg": require("@/assets/images/7/พิกัดสัตตะโลหะ/รากต้นใบทอง.jpg"),
+
+  //พิกัดสัตตะเทียน
+  "เทียนดำ.jpg": require("@/assets/images/7/พิกัดสัตตะเทียน/เทียนดำ.jpg"),
+  "เทียนแดง.jpg": require("@/assets/images/7/พิกัดสัตตะเทียน/เทียนแดง.jpg"),
+  "เทียนขาว.jpg": require("@/assets/images/7/พิกัดสัตตะเทียน/เทียนขาว.jpg"),
+  "เทียนข้าวเปลือก.jpg": require("@/assets/images/7/พิกัดสัตตะเทียน/เทียนข้าวเปลือก.jpg"),
+  "เทียนตาตั๊กแตน.jpg": require("@/assets/images/7/พิกัดสัตตะเทียน/เทียนตาตั๊กแตน.jpg"),
+  "เทียนสัตตบุษย์.jpg": require("@/assets/images/7/พิกัดสัตตะเทียน/เทียนสัตตบุษย์.jpg"),
+  "เทียนเยาวพาณี.jpg": require("@/assets/images/7/พิกัดสัตตะเทียน/เทียนเยาวพาณี.jpg")
+}
