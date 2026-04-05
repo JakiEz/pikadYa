abf31482217f7d106cf0690bfdadbcf5c4b93469
JakiEz <Jackietf4@gail.com>
2026-04-05 23:23:41 +0700

    scrollviewwww

--- app/medicine/[group]/index.tsx
+++ app/medicine/[group]/index.tsx
@@ -1,7 +1,7 @@
 //medicine/[group]/index.tsx
 import { LinearGradient } from "expo-linear-gradient";
 import { router, useLocalSearchParams } from "expo-router";
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity,ScrollView } from "react-native";
 import data from "../../../data/medicine.json";
 // import pic from "@assets/";
 
@@ -11,26 +11,27 @@ export default function GroupPage() {
   
   return (
     <LinearGradient colors={["#f5d742", "#f7e9b3"]} style={{ flex: 1, padding: 20 }}>
-      <Text style={{ fontSize: 60, fontWeight: "900", textAlign: "center", marginTop: 90 }}>
+      <Text style={{ fontSize: 60, fontWeight: "900", textAlign: "center", marginTop: 90}}>
         {group}
       </Text>
-
-      {categories.map((cat) => (
-        <TouchableOpacity
-          key={cat}
-          style={{
-            padding: 15,
-            backgroundImage: "",
-            backgroundColor: "#ddd",
-            marginTop: 15,
-            borderRadius: 20,
-            marginVertical: 15,
-          }}
-          onPress={() => router.push(`/medicine/${group}/${cat}` as any)} // Use type assertion
-        >
-          <Text style={{ textAlign: "center", fontSize: 20 }}>{cat}</Text>
-        </TouchableOpacity>
-      ))}
+      <ScrollView style={{ marginTop: 30, borderRadius: 20 }}>
+        {categories.map((cat) => (
+          <TouchableOpacity
+            key={cat}
+            style={{
+              padding: 15,
+              backgroundImage: "",
+              backgroundColor: "#ddd",
+              marginTop: 15,
+              borderRadius: 20,
+              marginVertical: 15,
+            }}
+            onPress={() => router.push(`/medicine/${group}/${cat}` as any)} // Use type assertion
+          >
+            <Text style={{ textAlign: "center", fontSize: 20 }}>{cat}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </LinearGradient>
   );
 }
